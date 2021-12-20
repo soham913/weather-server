23dee0fd0bcc63aa52c516968df2a8c0c0630585
soham913 <sohamsagade36@gmail.com>
2021-12-20 12:15:50 +0530

    Changes for Heroku.

--- public/js/script.js
+++ public/js/script.js
@@ -5,7 +5,7 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const location = inputValue.value;
-    fetch(`http://localhost:3000/weather?address=${location}`).then((res) => {
+    fetch(`/weather?address=${location}`).then((res) => {
     res.json().then((data) => {
             if(data.err) {
                 document.getElementById('result').innerHTML = data.err;
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode');
 const weather = require('./utils/weather')
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname,'../public')));
 
@@ -74,4 +75,4 @@ app.get('*', (req, res) => {
 
 
 
-app.listen(3000, () => console.log('Server up!'));
+app.listen(port, () => console.log('Server up!'));
